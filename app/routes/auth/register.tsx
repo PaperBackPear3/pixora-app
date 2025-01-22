2f599d822963bbcb15245412c8ae6f8d42a40460
PaperBackPear3 <fabioangeloni2001@gmail.com>
2025-01-22 01:26:18 +0100

    add card and Input for login and register

--- app/routes/auth/register.tsx
+++ app/routes/auth/register.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Button } from "~/components/ui/button";
+import { Card, CardContent, CardHeader } from "~/components/ui/card";
+import { Input } from "~/components/ui/input";
 
 const Register: React.FC = () => {
   const [email, setEmail] = useState("");
@@ -14,41 +16,77 @@ const Register: React.FC = () => {
   };
 
   return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <Button type="submit">Register</Button>
-      </form>
-    </div>
+    <Card>
+      <CardHeader>Login</CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <Input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <Input
+              type="text"
+              id="username"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <Input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <Button type="submit">Register</Button>
+        </form>
+      </CardContent>
+    </Card>
+    // <div className="login-container">
+    // <form onSubmit={handleSubmit}>
+    //   <div>
+    //     <label htmlFor="email">Email:</label>
+    //     <input
+    //       type="email"
+    //       id="email"
+    //       value={email}
+    //       onChange={(e) => setEmail(e.target.value)}
+    //       required
+    //     />
+    //   </div>
+    //   <div>
+    //     <label htmlFor="username">Username:</label>
+    //     <input
+    //       type="text"
+    //       id="username"
+    //       value={userName}
+    //       onChange={(e) => setUserName(e.target.value)}
+    //     />
+    //   </div>
+    //   <div>
+    //     <label htmlFor="password">Password:</label>
+    //     <input
+    //       type="password"
+    //       id="password"
+    //       value={password}
+    //       onChange={(e) => setPassword(e.target.value)}
+    //       required
+    //     />
+    //   </div>
+    //   <Button type="submit">Register</Button>
+    // </form>
+    // </div>
   );
 };
 
