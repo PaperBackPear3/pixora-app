4a347e532221a25c6b5fb51e8507ce7cd8505aa2
PaperBackPear3 <fabioangeloni2001@gmail.com>
2025-01-25 17:44:57 +0100

    TMP: no ssr

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,9 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false,
+  // return a list of URLs to prerender at build time
+  async prerender() {
+    return ["/", "/about"];
+  },
 } satisfies Config;
